de5f06ef7ef243efa8b2225cfa2405ccae2ce779
pirate-turtle <gldkzm@gmail.com>
2020-07-15 11:44:52 +0900

    BootStrap template 적용한 블로그 추가 사용 템플릿 https://startbootstrap.com/themes/clean-blog/

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,14 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from mysite.views import HomeView
 
-from bookmark.views import BookmarkLV, BookmarkDV
+# from bookmark.views import BookmarkLV, BookmarkDV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('', HomeView.as_view(), name='home'),
+    path('bookmark/', include('bookmark.urls')),
+    path('blog/', include('blog.urls')),
     # class-based views
-    path('bookmark/', BookmarkLV.as_view(), name='index'),
-    path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail')
+    # path('bookmark/', BookmarkLV.as_view(), name='index'),
+    # path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
 ]
