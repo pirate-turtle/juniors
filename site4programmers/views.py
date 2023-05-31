0c72c998ac39b51ffabcc25a06ae2bffefda95fb
Junghyeon Song <gldkzm@gmail.com>
2023-05-31 18:57:24 +0900

    인증 기능 추가 - 회원가입 - 로그인 - 로그아웃 - 비밀번호 변경

--- site4programmers/views.py
+++ site4programmers/views.py
@@ -1,4 +1,32 @@
 from django.views.generic import TemplateView
 
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+from django.contrib.auth import authenticate, login
+
+
 class HomeView(TemplateView):
     template_name='home.html'
+
+
+class UserJoinView(CreateView):
+    template_name = 'registration/join.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('join_done')
+
+    def form_valid(self, form):
+        # 가입한 유저를 DB에 등록하기 위해 부모 함수 호출
+        response = super().form_valid(form)
+
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+        user = authenticate(self.request, username=username, password=password)
+        login(self.request, user)
+
+        return response    
+    
+
+class UserJoinCompleteView(TemplateView):
+    template_name = 'registration/join_done.html'
