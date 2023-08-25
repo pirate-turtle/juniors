62af2d408bb7a2ba501ea86bde9d8c31973aab9e
Junghyeon Song <gldkzm@gmail.com>
2023-08-25 16:20:57 +0900

    게시글 기능 추가

---
+++ post/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(verbose_name='TITLE', max_length=50)
+    content = models.TextField('CONTENT')
+    create_dt = models.DateTimeField('CREATE DATE', auto_now_add=True)
+    modify_dt = models.DateTimeField('MODIFY DATE', auto_now=True)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField('CONTENT')
+    create_dt = models.DateTimeField('CREATE DATE', auto_now_add=True)
+    modify_dt = models.DateTimeField('MODIFY DATE', auto_now=True)
+
+    def __str__(self):
+        return self.content
